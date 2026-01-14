f4c56070ce01dbb7f42e5ea18eb42fc08302af6e
Guzmán González de Riancho <guzmangriancho@gmail.com>
2026-01-14 11:36:12 +0100

    feat: global exception handler

--- src/main/java/com/qaracter/novabank/exception/GlobalExceptionHandler.java
+++ src/main/java/com/qaracter/novabank/exception/GlobalExceptionHandler.java
@@ -1,4 +1,47 @@
 package com.qaracter.novabank.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleAccountNotFound(AccountNotFoundException exception) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Account not found");
+        response.put("timestamp", LocalDateTime.now());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidIbanException.class)
+    public ResponseEntity<Map<String, Object>> handleInvalidIban(InvalidIbanException exception) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Invalid IBAN");
+        response.put("timestamp", LocalDateTime.now());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidAmountException.class)
+    public ResponseEntity<Map<String, Object>> handleInvalidAmount(InvalidAmountException exception) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Invalid amount");
+        response.put("timestamp", LocalDateTime.now());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<Map<String, Object>> handleInsufficientBalance(InsufficientBalanceException exception) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Insuficient Balance");
+        response.put("timestamp", LocalDateTime.now());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }
