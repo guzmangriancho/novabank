858ca7b5bead95e0dbfdb95e12202cb62c3a3c3a
Guzmán González de Riancho <guzmangriancho@gmail.com>
2026-01-14 11:12:02 +0100

    project initialization

---
+++ src/main/java/com/qaracter/novabank/NovabankApplication.java
@@ -0,0 +1,13 @@
+package com.qaracter.novabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qaracter/novabank/NovabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qaracter.novabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
